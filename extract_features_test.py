8d65419b424258305a19d965be375922e4a4beb3
zhangchong <zhangchong@xiaoduotech.com>
2019-01-07 11:20:35 +0800

    word token

--- extract_features_test.py
+++ extract_features_test.py
@@ -60,6 +60,42 @@ def get_cls_embedding(bert_json_dir, raw_data_dir, layer_index, dest_dir):
     save_readable_jsons(check_file, index_sentences)
 
 
+def get_tokens_embedding(bert_json_dir,raw_data_dir, layer_index, dest_dir):
+    raw_data_df = pd_reader(raw_data_dir, 0 if raw_data_dir.endswith('csv') else None, [0, 1, 2], drop_dup_axis=2)
+    mat_data = None
+    base_name = Path(raw_data_dir).name
+    base_name = base_name[:base_name.rindex('.')]
+    index_sentences = defaultdict(str)
+    if not Path(dest_dir).is_dir():
+        Path(dest_dir).mkdir(parents=True, exist_ok=True)
+    out_file = Path(dest_dir).joinpath('{}_{}.npy'.format(base_name, layer_index))
+    check_file = Path(dest_dir).joinpath('{}_{}.json'.format(base_name, layer_index))
+    with open(bert_json_dir, 'r') as f:
+        for line in f:
+            setence_tokens = json.loads(line)
+            line_index = setence_tokens['linex_index']
+            features = setence_tokens['features']
+            sentence_emb = None
+            for i in range(min(35, len(features))):
+                token = features[i]
+                token_name = token['token']
+                index_sentences[line_index] = index_sentences[line_index] + token_name
+                if token_name not in ['[CLS]', '[SEP]']:
+                    for layer_token in token['layers']:
+                        if layer_token['index'] == layer_index:
+                            value = layer_token['values']
+                            if mat_data is None:
+                                mat_data = np.zeros((raw_data_df.shape[0], 35, len(value)), dtype='float16')
+                            if sentence_emb is None:
+                                sentence_emb = np.zeros((35, len(value)), dtype='float16')
+                            sentence_emb[i] = value
+            mat_data[line_index] = sentence_emb
+
+    np.save(out_file, mat_data)
+    index_sentences = OrderedDict(sorted(index_sentences.items()))
+    save_readable_jsons(check_file, index_sentences)
+
+
 def main(_):
     get_cls_embedding(FLAGS.bert_json_dir, FLAGS.raw_data_dir, FLAGS.layer_index, FLAGS.dest_dir)
 
